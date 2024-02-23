b154b11a54fb22da65f02bc2b862fbe94d7a4c7d
valenmadonia <valentinamadonia7@gmail.com>
2024-02-23 16:49:48 -0300

    svm integration

---
+++ embedia/layers/svm/svc_layer.py
@@ -0,0 +1,51 @@
+from embedia.layers.data_layer import DataLayer
+from embedia.model_generator.project_options import ModelDataType
+
+class SVC_layer(DataLayer):
+        
+    def __init__(self, model, layer, options=None, **kwargs):
+        super().__init__(model, layer, options, **kwargs)
+        self.struct_data_type = 'svc_layer_t'
+    
+    def functions_init(self):
+
+        struct_type = self.struct_data_type
+        name = self.name
+    
+        init_svc_layer = f'''
+        {struct_type} init_{name}_data(void){{
+        char * kernel_type = "{self.model.kernel.lower()}";
+        int degree = {self.model.degree};
+        double gamma = {self.model.gamma};
+        double  coef0 = {self.model.coef0};
+        double C = {self.model.C};
+        double rho[] = {'{' + ', '.join(map(str, self.model.intercept_)) + '}'};
+        int nSV[] = {'{' + ', '.join(map(str, self.model.n_support_)) + '}'};
+        double SV[][] = {{'''
+        for vector in self.model.support_vectors_:
+            init_svm_layer += f'        {{' + ', '.join(map(str, vector)) + '}},\n'
+        init_svm_layer += '''    }};
+        double dual_coef[][] = {{ '''
+        for row in self.model.dual_coef_:
+            init_svm_layer += f'        {{' + ', '.join(map(str, row)) + '}},\n'
+        
+        init_svm_layer += f'''    }};
+
+        svc_layer_t layer = {{
+                kernel_type,
+                degree,
+                gamma,
+                coef0,
+                C,
+                {{{{sizeof(rho) / sizeof(rho[0])}}, rho}},
+                {{{{sizeof(nSV) / sizeof(nSV[0])}}, nSV}},
+                {{{{sizeof(SV) / sizeof(SV[0])}}, SV}},
+                {{{{sizeof(dual_coef) / sizeof(dual_coef[0])}}, dual_coef}}
+        }};
+            return layer;
+        }}
+        '''
+        return init_svc_layer
+    
+    def predict(self, input_name, output_name):
+        return f'''svc_layer({self.name}_data, {input_name}, &{output_name});'''
---
+++ embedia/model_generator/project_options.py
@@ -0,0 +1,45 @@
+class ModelDataType:
+    (FLOAT, FIXED32, FIXED16, FIXED8, BINARY, BINARY_FIXED32, BINARY_FLOAT16) = (0, 1, 2, 3, 4, 5, 6)
+
+    SIZES = [32, 32, 16, 8, 32, 32, 16]
+
+    def get_size(dt):
+
+        return ModelDataType.SIZES[dt]
+
+
+class ProjectType:
+    (C, CPP, ARDUINO, CODEBLOCK) = (0, 1, 2, 3)
+
+
+class ProjectFiles:
+    (LIBRARY, MAIN, MODEL) = (1, 2, 4)
+    (ALL) = {LIBRARY, MAIN, MODEL}
+
+
+class DebugMode:
+    (DISCARD, DISABLED, HEADERS, DATA) = (-1, 0, 1, 2)
+
+
+class BinaryBlockSize:
+    (Bits8, Bits16, Bits32, Bits64) = (0, 1, 2, 3)
+
+
+class UnimplementedLayerAction:
+    (FAILURE, IGNORE_ALL, IGNORE_KNOWN) = (0, 1, 2)
+
+
+class ProjectOptions:
+    def __init__(self):
+        self.embedia_folder = None
+        self.project_type = ProjectType.C
+        self.data_type = ModelDataType.FLOAT
+        self.baud_rate = 9600
+        self.example_data = None
+        self.example_labels = None
+        self.files = ProjectFiles.ALL
+        self.debug_mode = DebugMode.DISABLED
+        self.clean_output = False
+        self.normalizer = None
+        self.tamano_bloque = BinaryBlockSize.Bits8
+        self.on_unimplemented_layer = UnimplementedLayerAction.IGNORE_KNOWN
